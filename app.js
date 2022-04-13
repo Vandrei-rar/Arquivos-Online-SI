bbd3deeeec8e90158f1950757fb9fab4ef71cac8
Vandrei-rar <vandreiribeiro@gmail.com>
2022-04-13 10:16:47 -0300

    Inicio projeto

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+// const session = require('express-session')
+const route = require('./routes/web')
+
+const app = express()
+
+// Set template
+app.engine('handlebars', exphbs({defaultLayout : 'main'}))
+app.set('view engine', 'handlebars')
+
+// Config session
+// app.use(session({secret: 'c6712598023cfa20882b2667006f6c23d8adad65'}))
+
+app.use('/', route)
+
+app.listen(3000, () => {
+    console.log("Servidor rodando em http://localhost:3000");
+})
---
+++ routes/web.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const router = express.Router()
+const adm = require('./adm')
+
+router.get('/', (req, res) => {
+    res.send("Oi")
+})
+
+// Acesso restrito
+router.use('/adm', adm)
+
+module.exports = router
