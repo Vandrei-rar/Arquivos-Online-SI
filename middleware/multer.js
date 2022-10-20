1db1f1e958888cf7e1944d25fe5d461a757faf9c
Vandrei-rar <vandreiribeiro@gmail.com>
2022-10-20 16:14:06 -0300

    V1 dashboard

--- middleware/multer.js
+++ middleware/multer.js
@@ -24,19 +24,19 @@ module.exports = (multer({
 
     }),
 
-    fileFilter: (req, file, cb) => { // Filtro para tipos de arquivos diferentes, o que pode ou não ser anexado.
+    // fileFilter: (req, file, cb) => { // Filtro para tipos de arquivos diferentes, o que pode ou não ser anexado.
 
-        // Formatos aceitos podem ser inseridos dentro do vetor, ao invés de fazer vários IF's.
-        const isAccepted = ['image/png', 'image/jpg', 'image/jpeg', 'application/pdf', 'text/plain'].find( formatoAceito => formatoAceito == file.mimetype );
+    //     // Formatos aceitos podem ser inseridos dentro do vetor, ao invés de fazer vários IF's.
+    //     const isAccepted = ['image/png', 'image/jpg', 'image/jpeg', 'application/pdf', 'text/plain'].find( formatoAceito => formatoAceito == file.mimetype );
 
-        // O formato do arquivo bateu com algum aceito?
-        if(isAccepted){
-            // Executamos o callback com o segundo argumento true (validação aceita)
-            return cb(null, true);
-        }
+    //     // O formato do arquivo bateu com algum aceito?
+    //     if(isAccepted){
+    //         // Executamos o callback com o segundo argumento true (validação aceita)
+    //         return cb(null, true);
+    //     }
             
-        // Se o arquivo não bateu com nenhum aceito, executamos o callback com o segundo valor false (validação falhouo)
-        return cb(null, false);
+    //     // Se o arquivo não bateu com nenhum aceito, executamos o callback com o segundo valor false (validação falhouo)
+    //     return cb(null, false);
 
-    }
+    // }
 }))
--- routes/adm.js
+++ routes/adm.js
@@ -2,70 +2,17 @@ const express = require('express')
 const multer = require('../middleware/multer')
 const router = express.Router()
 const authcontroller = require('../controller/authcontroller')
+const filecontroller = require('../controller/filecontroller')
 const sessionCheck = require('../middleware/sessionCheck')
-const database = require('../database/connection')
-const mysql = require('mysql2/promise')
-const flash = require('express-flash')
-const fs = require('fs')
-
 
 router.get('/', authcontroller.iflogin) // Ao entrar na rota principal /adm/, há uma verificação de sessão, se há ou não alguma sessão aberta.
-
-
 
 router.post('/login', authcontroller.login) // Rota de login
 router.post('/logout', authcontroller.logout ) // Rota de logout
 
 
-router.get('/managefile', sessionCheck.check, function(req, res) {
-    res.render('./admin/managefile')
-})
-
-// CRUD de arquivos.
-router.get('/managefile/view', sessionCheck.check, async function(req, res, next){
-    let result,
-        qry = "SELECT arquivos.titulo, arquivos.criado_em, materias.nome, materias.fk_professor FROM arquivos INNER JOIN materias, professores WHERE fk_materia = materias.id AND materias.fk_professor = professores.chapa AND professores.nome = " + mysql.escape(req.session.login.nome) + ";"
-
-    result = await database.queryCmd(qry)
-    // SELECT id, titulo FROM arquivos;
-
-    // console.log(result);
-    res.render('./admin/managefile', {findResult: result}) // Chamando a página managefile passando o parâmetro 'result' para manipular internamente no handlebars.
-
-})
-
-    // Adicionando arquivos
-router.post('/managefile/create', sessionCheck.check, multer.single('file'), async function(req, res){
-    if (req.file) {
-
-        // Vetor constante materiaDetector serve para identificar qual a matéria correspondente do professor logado na sessão atual do sistema. Para realizar o insert corretamente e isolar arquivos por matérias e professores. Dessa forma um professor não consegue ver os arquivos dos outros, mantendo a ordem e organização.
-        // Utiliza-se um vetor na mesma lógica do arquivo "authcontroller", a resposta do BD chega como um objeto, portanto deve-se manipular com um vetor.
-        const [materiaDetector] = await database.queryCmd("SELECT materias.FK_PROFESSOR FROM materias INNER JOIN professores WHERE materias.FK_PROFESSOR = professores.CHAPA AND professores.nome = " + mysql.escape(req.session.login.nome) + ";")
-        //console.log(materiaDetector);
-
-
-        // Inserindo arquivos com seus nomes e respectivos locais no servidor, com a separação por matéria.
-        await database.queryCmd('INSERT INTO arquivos (titulo, arquivo, fk_materia) VALUES (' + mysql.escape(req.file.originalname) + ',' + mysql.escape(req.file.destination) + ',' + await materiaDetector['FK_PROFESSOR'] + ');')
-
-        req.flash('successUpload', 'Arquivo armazenado com sucesso!') // Mensagem flash de sucesso para retorno para o usuário.
-        return res.render('./admin/managefile', {successUpload: req.flash('successUpload')})
-        // return res.send(req.file)
-    }else{
-        req.flash('errorUpload', 'Tipo de arquivo não suportado!')// Mensagem flash de erro para retorno para o usuário.
-        return res.render('./admin/managefile', {errorUpload: req.flash('errorUpload')}) 
-    }
-
-})
-
-router.post('/managefile/delete/:titulo', sessionCheck.check, async function(req, res){ // Rota post para enviar a informação de deletar
-    
-    let fileName = req.params.titulo // Pelos parametros da URL captura-se o que vem no :titulo enviado pelo managefile.handlebars
-
-    await fs.unlinkSync('./uploads/files/' + fileName) // Função assíncrona para exclusão de arquivos no diretório estático.
-    await database.queryCmd("DELETE FROM arquivos WHERE titulo = " + mysql.escape(fileName)) // Função assíncrona para exclusão do registro no banco de dados.
-
-    res.redirect('/adm/managefile/view')
-
-})
+router.get('/managefile', sessionCheck.check, filecontroller.viewFiles)
+router.post('/managefile/create', sessionCheck.check, multer.single('file'), filecontroller.createFiles )
+router.post('/managefile/delete/:titulo', sessionCheck.check, filecontroller.destroyFiles)
 
 module.exports = router
